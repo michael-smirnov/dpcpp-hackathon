e55a7cc2dfb2e6d79377ee4deff552b8c8ae7a9d
Michael Smirnov <michael.smirnov@intel.com>
2019-11-28 20:27:41 +0300

    launch script fixed

--- launch.sh
+++ launch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-dpcpp -std=c++17 $1 -o builds/$1.exe
+mkdir -p builds
+dpcpp -std=c++17 -O2 $1 -o builds/$1.exe
 echo Build Passed
-python run.py data/data.txt builds/$1.exe 60
+python run.py data.txt builds/$1.exe 60
